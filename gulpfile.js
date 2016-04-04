411a763f5ce351a790c32e81a740a6e56d55c42a
Daniel McDermott <dmcdermott@forumone.com>
2016-04-04 09:40:06 -0700

    Added Angular Tree Control, fix to versions, gulp

--- gulpfile.js
+++ gulpfile.js
@@ -115,4 +115,4 @@ gulp.task('livereload', function() {
  * Gulp tasks
  */
 gulp.task('build', ['usemin', 'build-assets', 'build-custom']);
-gulp.task('default', ['build', 'webserver', 'livereload', 'watch']);
+gulp.task('default', ['build', 'livereload', 'watch']);
